4e53310183dc79688fbe3a41f9ceeb4b20a98577
Kryptonhydrit <kryptonhydrit@kryptonhydrit.de>
2026-04-19 21:04:40 +0200

    Bump All the Mons server version to 1.0.0-rc.2

--- servermanager.sh
+++ servermanager.sh
@@ -8,8 +8,8 @@ BLUE="\033[0;34m"
 NC="\033[0m"
 
 # Variables
-_SERVER_VERSION="1.0.0-rc.1"
-_SERVER_DOWNLOAD_PATH="7946/174"
+_SERVER_VERSION="1.0.0-rc.2"
+_SERVER_DOWNLOAD_PATH="7951/179"
 _SERVER_FILES="ServerFiles-${_SERVER_VERSION}.zip"
 
 echo -e "${BLUE}> [DEBUG] ATMons - Server version: ${_SERVER_VERSION}${NC}"
